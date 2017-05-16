ff27d2745d96d910bf3cd0c757419b666488acbc
Solomon <Solomon>
2017-05-16 13:33:10 -0400

    I have done the bonus problem. I wanted to updated it.

--- index.js
+++ index.js
@@ -12,33 +12,31 @@ const handleSubmit = (ev) => {
   ev.preventDefault()
   const form = ev.target
   const details = document.querySelector('.details')
-
-  const personName = form.personName.value
-  const hairColor = form.hairColor.value
-  const age = form.age.value
-  const birthplace = form.birthplace.value
-
-  const colorDiv = renderColor(hairColor)
-//console.log("1")
-  const unorderList = document.createElement('ul')
-  const person = document.createElement('li')
-  person.textContent = 'Name: ' + personName
-  const hair = document.createElement('li')
-  hair.textContent = 'Hair Color: '
-  const personAge = document.createElement('li')
-  personAge.textContent = 'Age: ' + age
-  const birth = document.createElement('li')
-  birth.textContent = 'Birthplace: ' + birthplace
-//console.log("2")
-  unorderList.appendChild(person)
-  unorderList.appendChild(hair)
-  unorderList.appendChild(colorDiv)
-  unorderList.appendChild(personAge)
-  unorderList.appendChild(birth)
-//console.log("3")
-  details.appendChild(unorderList)
-  
-  
+  listUnorder(form, details)
 }
+  
+  const listUnorder = (form, details) => {
+    const personName = form.personName.value
+    const hairColor = form.hairColor.value
+    const age = form.age.value
+    const birthplace = form.birthplace.value
+    const unorderList = document.createElement('ul')
+    const person = document.createElement('li')
+    person.textContent = 'Name: ' + personName
+    const hair = document.createElement('li')
+    const colorDiv = renderColor(hairColor)
+    hair.textContent = 'Hair Color: '
+    const personAge = document.createElement('li')
+    personAge.textContent = 'Age: ' + age
+    const birth = document.createElement('li')
+    birth.textContent = 'Birthplace: ' + birthplace
+    unorderList.appendChild(person)
+    unorderList.appendChild(hair)
+    unorderList.appendChild(colorDiv)
+    unorderList.appendChild(personAge)
+    unorderList.appendChild(birth)
+    details.appendChild(unorderList)
+    return unorderList
+  }
 
 personForm.addEventListener('submit', handleSubmit)
